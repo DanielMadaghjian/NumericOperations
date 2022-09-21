f64d5c41d909ab55f45e7a579fec8f43cfcf788b
Daniel Madaghjian <danielmadaghjian@gmail.com>
2022-09-21 17:40:35 +0100

    added more files

--- testing.py
+++ testing.py
@@ -1,4 +1,13 @@
-import multiply
+from NumericOperations import multiply
+from NumericOperations import division
+from NumericOperations import addition
+from NumericOperations import subtraction
 
 def test_mult():
 	assert multiply(10,11) == 110
+def test_div():
+	assert division(10,2) == 5
+def test_add():
+	assert addition(10,11) == 21
+def test_sub():
+	assert subtraction(10,11) == -1
